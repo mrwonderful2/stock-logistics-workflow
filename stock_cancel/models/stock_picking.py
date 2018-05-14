3fc5fe5966b083677778010e8c7f3ca08d653e70
aaron <ahenriquez@eficent.com>
2018-05-14 12:00:50 +0200

    [IMP]short style headers as recommended in maintainer tools

--- stock_cancel/models/stock_picking.py
+++ stock_cancel/models/stock_picking.py
@@ -1,27 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2012 Andrea Cometa All Rights Reserved.
-#                       www.andreacometa.it
-#                       openerp@andreacometa.it
-#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Ported to new API by Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012 Andrea Cometa
+# Copyright 2013 Agile Business Group sagl
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, models, exceptions, _
 
